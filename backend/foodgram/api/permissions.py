1a8f131f15898c4c8e68dd9819c84458729479de
iwanttobe00 <xxhh0@mail.ru>
2023-05-02 01:44:00 +0400

    ss

--- backend/foodgram/api/permissions.py
+++ backend/foodgram/api/permissions.py
@@ -6,6 +6,6 @@ class AuthorPermission(BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return (
-            request.method in SAFE_METHODS 
+            request.method in SAFE_METHODS
             or obj.author == request.user
-    )
+        )
