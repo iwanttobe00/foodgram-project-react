ca1e589fd72d1b4596c9f690439703738340ef7d
iwanttobe00 <xxhh0@mail.ru>
2023-05-03 22:32:16 +0400

    ss

--- backend/foodgram/recipes/models.py
+++ backend/foodgram/recipes/models.py
@@ -1,7 +1,7 @@
 from colorfield.fields import ColorField
 from django.contrib.auth import get_user_model
 from django.core.validators import (
-    MaxValueValidator, MinValueValidator, RegexValidator
+    MinValueValidator, RegexValidator
 )
 from django.db import models
 from django.db.models import UniqueConstraint
